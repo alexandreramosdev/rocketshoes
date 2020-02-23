4d6ac20179079958955bca7231d5def5202c9ade
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-02-23 03:04:51 -0300

    create cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,70 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <div />;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QTD</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-nike-revolution-5-masculino/26/HZM-1731-026/HZM-1731-026_detalhe1.jpg"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tênis Mass</strong>
+              <span>R$99,99</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#663399" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#663399" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$198,88</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#663399" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$1900,55</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
